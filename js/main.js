c696b3cfada4fffb6f8bedf6ef6af0c9c631565a
Kim Oxlund <hello@kimoxlund.com>
2014-03-22 20:00:42 -0700

    Started sorting stuff
    Signed-off-by: Kim Oxlund <hello@kimoxlund.com>

--- js/main.js
+++ js/main.js
@@ -1,15 +1,35 @@
 $(function(){
 
-    var s = skrollr.init();
+
 
     // Add nav toggle class
     $('.toggle-nav').click(function(){
         $('body').toggleClass('nav-open');
     });
 
-    $('.more').click(function(){
-        $(this).closest('.project').find('.project-more').toggleClass('open');
+    var currentProject = 1;
+
+    var numProjects = $('.screen').length;
+
+    var scrollPercentage = 1;
+
+    $('.btn').click(function(){
+        var visibility = $(this).data('cat');
+
+        if(visibility === 'web'){
+            $('.motion').remove();
+        } else if(visibility === 'video') {
+            $('.web').remove();
+        }
+
+        $(".screen").each(function(i){
+
+            $(this).attr("style","z-index: " + (numProjects - i));
+            $(this).attr("data-" + (i * 100) + "p","opacity:1; display: block;");
+            $(this).attr("data-" + ((i * 100) + 70) + "p","opacity:0; display: none;");
+
+        });
+
+        var s = skrollr.init();
     });
-
-
 });
